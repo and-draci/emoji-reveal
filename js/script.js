60abb4b441f0d48524b15f8c226b111eacdbb945
and_draci <draciannanderson@gmail.com>
2022-11-16 00:47:08 -0500

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
+123;
+
+document.addEventListener("keydown", function (e) {
+  //console.log(e.key);
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "😃";
+  } else if (key === "2") {
+    box2.innerText = "❤️";
+  } else if (key === "3") {
+    box3.innerText = "🌍";
+  }
+});
+
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
